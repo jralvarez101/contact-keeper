d8a592d3c7fd9362f0e2fb27204b545b3bda9676
martin <alvarez.martinjr@gmail.com>
2021-08-23 22:37:41 -0700

    delete contact functionality

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { FaEnvelopeOpen, FaPhone } from "react-icons/fa";
 import PropTypes from "prop-types";
+import ContactContext from "../../context/contact/contactContext";
 
 export const ContactItem = ({ contact }) => {
+  // Initialize context
+  const contactContext = useContext(ContactContext);
+
+  // Pull deleteContact function from contactContext
+  const { deleteContact } = contactContext;
+
   const { id, name, email, phone, type } = contact;
+
+  const onDelete = () => {
+    // we have access to the id that we are pulling out from contact.
+    deleteContact(id);
+  };
   return (
     <div className="card bg-light">
       <h3 className="text-primary text-left">
@@ -34,7 +46,9 @@ export const ContactItem = ({ contact }) => {
       </ul>
       <p>
         <button className="btn btn-dark btn-sm">Edit</button>
-        <button className="btn btn-danger btn-sm">Delete</button>
+        <button className="btn btn-danger btn-sm" onClick={onDelete}>
+          Delete
+        </button>
       </p>
     </div>
   );
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -57,7 +57,9 @@ const ContactState = (props) => {
     dispatch({ type: ADD_CONTACT, payload: contact });
   };
 
-  // --- Delete Contact
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
 
   // --- Set Current Contact
 
@@ -75,6 +77,7 @@ const ContactState = (props) => {
       value={{
         contacts: state.contacts,
         addContact,
+        deleteContact,
       }}
     >
       {props.children}
